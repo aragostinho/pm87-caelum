1ddb22f531fd8c8def5f553b29f41d5eacff468e
andre agostinho <andre_ragostinho@hotmail.com>
2013-12-17 19:47:15 -0200

    Primeiro commit remoto

---
+++ ConsoleApplicationTest/ConsoleApplicationTest/ConsoleApplication1/Teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Teste
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de teste de uso do git");
+        }
+    }
+}
